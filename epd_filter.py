72cbbfa8b2b249806460998303cf61a50607bda6
Jay Honnold <jayhonnold@gmail.com>
2024-01-27 16:19:07 -0700

    Setup multi-threaded filtering

--- epd_filter.py
+++ epd_filter.py
@@ -1,44 +1,74 @@
-import os
-import re
-import sys
+import os, re, sys, concurrent.futures
+from multiprocessing import freeze_support, cpu_count
 from uci_engine import Engine
+from tqdm import tqdm
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
 W_Min = 400
 W_Max = 600
 
-def filter(testfile, output):
-    def win_analysis(engine, fen, depth = 22):
-        engine.ucinewgame()
+class Filterer:
+    def __init__(self):
+        self.engine = os.listdir("engines/")[0]
+
+    def filter(self, fens):
+        result_fens = []
+
+        engine = Engine(self.engine, "engines/")
         engine.set_option('UCI_ShowWDL', 'true')
-        engine.position(fen)
-        engine.go(depth=depth)
 
-        w = 0
+        for fen in fens:
+            engine.ucinewgame()
+            engine.position(fen)
+            engine.go(depth=22)
 
-        while True:
-            response = engine.readline()
-            match = re.search(' wdl (\d+) (\d+) (\d+) ', response)
-            if match: w = int(match.group(1));
-            if response.startswith('bestmove'): break;
+            w = 0
 
-        return w
+            while True:
+                response = engine.readline()
+                match = re.search(' wdl (\d+) (\d+) (\d+) ', response)
+                if match: w = int(match.group(1));
+                if response.startswith('bestmove'): break;
 
-    engines = os.listdir("engines/")[:1]
-    engine = Engine(engines[0], "engines/")
+            if w >= W_Min and w <= W_Max:
+                result_fens.append(fen)
 
-    kept = 0
+        engine.quit()
 
-    with open(output, 'w') as out:
-        with open(testfile, 'r') as fens:
-            for count, fen in enumerate(fens):
-                w = win_analysis(engine, fen)
-
-                if w >= W_Min and w <= W_Max:
-                    kept += 1
-                    out.write(fen)
-
-                if (count+1) % 1000 == 0:
-                    print(f"Kept {kept} fen(s) of {count+1}")
+        return result_fens
 
 if __name__ == "__main__":
-    filter(sys.argv[1], sys.argv[2])
+    freeze_support()
+
+    fens = []
+    with open(sys.argv[1]) as f:
+        for line in f:
+            fens.append(line)
+
+    filter = Filterer()
+
+    workers = cpu_count()
+    num_fens = len(fens)
+
+    print(f"Loaded {num_fens} fen(s)")
+
+    fenschunked = list(chunks(fens, 256))
+
+    res = []
+    futures = []
+
+    with tqdm(total=len(fenschunked), smoothing=0, miniters=1) as pbar:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as e:
+            for entry in fenschunked:
+                futures.append(e.submit(filter.filter, entry))
+
+            for future in concurrent.futures.as_completed(futures):
+                pbar.update(1)
+                res += future.result()
+
+    with open(sys.argv[2], 'w') as f:
+        f.writelines(res)
